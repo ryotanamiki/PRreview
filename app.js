b8cabb786e65c392dd29f3d68e7feb7441d2e4ea
r.namiki <aaasti555@gmail.com>
2023-09-27 10:32:13 +0900

    課題05--レビュー追加機能の実装

--- app.js
+++ app.js
@@ -44,6 +44,46 @@ con.query('SELECT * FROM personas', (err, results) => {
         });
     });
 
+    app.get('/create', (req, res) => {
+        res.sendFile(path.join(__dirname, '/views/form.ejs'))
+    });
+
+app.get('/edit/:id', (req, res) => {
+  const sql = "SELECT * FROM personas WHERE id = ?";
+  con.query(sql, [req.params.id], function (err, result, fields) {
+    if (err) throw err;
+    res.render('edit', {
+      personas: result
+    });
+  });
+});
+
+    app.post('/update/:id', (req, res) => {
+  const sql = "UPDATE personas SET ? WHERE id = " + req.params.id;
+  con.query(sql, req.body, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.redirect('/');
+  });
+    });
+
+// 新しいレビューを追加
+app.post('/addReview', (req, res) => {
+    const { username, age, rating, reason } = req.body;
+
+    con.query('INSERT INTO personas (username, age, rating, reason) VALUES (?, ?, ?, ?)',
+        [username, age, rating, reason],
+        (err, result) => {
+            if (err) {
+                console.error(err);
+                res.send('レビューの追加に失敗しました。');
+            } else {
+                console.log('新しいレビューが追加されました。');
+                res.redirect('/');
+            }
+        });
+});
+
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
